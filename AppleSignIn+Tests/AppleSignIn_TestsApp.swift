09f2ee897943fcf5b5b177b6d2cff738e9884f4d
Ardyan <ardyanrizki@icloud.com>
2023-10-22 16:01:31 +0700

    Initial commit

---
+++ AppleSignIn+Tests/AppleSignIn_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSignIn_TestsApp.swift
+//  AppleSignIn+Tests
+//
+//  Created by Muhammad Rizki Ardyan on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleSignIn_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleSignIn+Tests/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleSignIn+Tests
+//
+//  Created by Muhammad Rizki Ardyan on 22/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
